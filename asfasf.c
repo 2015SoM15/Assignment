091e3e1bddcd8014dbd16ccb22d3839925f29316
yongjini100 <yongjini100@naver.com>
2015-05-08 18:15:55 +0900

    원래있던거 지우고 다른함수 만들어봤다.

--- asfasf.c
+++ asfasf.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 int main()
-{
+/*{
 int i=0;
 int sum=0;
 for(i=0; i<=10; i++)
 	sum=sum+i;
 printf("%d",sum);
 return 0;
+}
+*/
+int main()
+{
+	printf("md는 우리의 적"\n);
+	return 0;
+
 }
